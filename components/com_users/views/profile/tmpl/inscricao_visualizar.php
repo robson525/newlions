db10c9f94eceaf64c93a27f8130cf3d089d7ba0b
robson.silva <robson.c.silva@capgemini.com>
2020-01-09 09:46:24 -0300

    Remover dados de pagamento

--- components/com_users/views/profile/tmpl/inscricao_visualizar.php
+++ components/com_users/views/profile/tmpl/inscricao_visualizar.php
@@ -13,38 +13,39 @@
 
 <hr>
 
-<h3 class="center bold">Pagamento</h3>
-<div class="row-fluid">
-    <div class="span5">
-        <h4 class="center bold">Dados Bancarios</h4>
-        <p class="center">Banco do Brasil</p>                                               
-        <p class="center">
-            Agência: 3506-8 <br>
-            Conta Corrente: 42056-5                 
-        </p>
-        <p class="center">
-            Titular: DG Rita: BANCO DO BRASIL.
-        </p>
-    </div>
-    <div class="span5">
-        <h4 class="center bold">Valores das Incrições</h4>    
-        <table class="table">
-            <tr>
-                <td>Até dia 30/12/2019</td>
-                <td>R$ 300,00</td>
-            </tr>
-            <tr>
-                <td>Até dia 28/02/2020</td>
-                <td>R$ 320,00</td>
-            </tr>
-            <tr>
-                <td>Até dia 30/04/2020</td>
-                <td>R$ 350,00</td>
-            </tr>
-        </table>
-    </div>
-</div>
-
+<div style="display: none;">
+	<h3 class="center bold">Pagamento</h3>
+	<div class="row-fluid">
+		<div class="span5">
+			<h4 class="center bold">Dados Bancarios</h4>
+			<p class="center">Banco do Brasil</p>                                               
+			<p class="center">
+				Agência: 3506-8 <br>
+				Conta Corrente: 42056-5                 
+			</p>
+			<p class="center">
+				Titular: DG Rita: BANCO DO BRASIL.
+			</p>
+		</div>
+		<div class="span5">
+			<h4 class="center bold">Valores das Incrições</h4>    
+			<table class="table">
+				<tr>
+					<td>Até dia 30/12/2019</td>
+					<td>R$ 300,00</td>
+				</tr>
+				<tr>
+					<td>Até dia 28/02/2020</td>
+					<td>R$ 320,00</td>
+				</tr>
+				<tr>
+					<td>Até dia 30/04/2020</td>
+					<td>R$ 350,00</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+<div>
 <hr>
 
 <?= $this->loadTemplate('comprovante');  ?>
