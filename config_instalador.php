b91df24f15b391ec9d8b9973fe0568cfc4822803
robson525 <rob.engcomp@gmail.com>
2020-01-28 00:31:51 -0300

    Atualização do joomla

---
+++ config_instalador.php
@@ -0,0 +1,29 @@
+<?php
+
+define('MAIL_SERVIDOR', 'smtp.lionsdla6.com.br');
+define('MAIL_USUARIO', 'convencao=lionsdla6.com.br');
+define('MAIL_EMAIL', 'convencao@lionsdla6.com.br');
+define('MAIL_SENHA', 'Fortunamajor1*');
+define('MAIL_PORTA', '587');
+define('MAIL_CRIPT', '');
+define('MAIL_IMAP', 'mboxc.f1.ultramail.com.br');
+	
+define('DB_SERVIDOR', 'mysql.lionsdla6.com.br');
+define('DB_USUARIO', 'lionsdla6');
+define('DB_NOME', 'lionsdla6');
+define('DB_SENHA', 'Psherman42WallabyWaySydney');
+define('PREFIXO', 'jom');
+
+define('FTP_SERVIDOR', 'ftp.lionsdla6.com.br');
+define('FTP_USUARIO', 'lionsdla6');
+define('FTP_SENHA', 'Psherman42WallabyWaySydney');
+
+define('DOMINIO', 'www.lionsdla6.com.br');
+define('DIRETORIO', 'www/');
+define('DIR', '');
+define('SSL', "false");
+define('SITENOME', '');
+define('KUNDE', 'true');
+define('APP_SENHA', 'Caodoinferno1*');
+		
+?>
